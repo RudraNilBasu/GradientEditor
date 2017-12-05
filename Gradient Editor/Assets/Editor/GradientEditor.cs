113743e9d9da8b44aa2435db5f455496fa5f2d4f
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2017-12-05 14:35:35 +0530

    GradientEditor: Implemented CustomGradient class
    CustomGradient: Class which contains the gradient, currently done
    via a single gradient from white to black
    
    GradientDrawer: Specifies the GUI properties of the CustomGradient
    class, which displays the label and gradient and displays a window
    of type GradientEditor when it is clicked
    
    Signed-off-by: RudraNilBasu <rudra.nil.basu.1996@gmail.com>

---
+++ Gradient Editor/Assets/Editor/GradientEditor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GradientEditor : EditorWindow {
+
+    private void onEnable()
+    {
+        titleContent.text = "Gradient Editor";
+    }
+}
