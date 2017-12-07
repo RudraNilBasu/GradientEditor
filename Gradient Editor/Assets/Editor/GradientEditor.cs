3b3963d99319c6f84d755fa14dcdab97edfb263c
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2017-12-07 20:12:17 +0530

    ColourKey: Add random colurs in gradient
    The Custom Gradient contains a list of ColourKey elements which
    are taken from the Editor window (currently, they are randomly
    generated on clicking on the window) and the resultant texture
    is created by blending the values between the colours in the
    ColourKey list.
    
    Signed-off-by: RudraNilBasu <rudra.nil.basu.1996@gmail.com>

--- Gradient Editor/Assets/Editor/GradientEditor.cs
+++ Gradient Editor/Assets/Editor/GradientEditor.cs
@@ -7,11 +7,29 @@ public class GradientEditor : EditorWindow {
 
     CustomGradient gradient;
     const int borderSize = 10;
+    const float keyWidth = 10;
+    const float keyHeight = 20;
 
     public void OnGUI()
     {
+        Event guiEvent = Event.current;
         Rect gradientPreviewRect = new Rect(borderSize, borderSize, position.width - borderSize * 2, 25);
         GUI.DrawTexture(gradientPreviewRect, gradient.GetTexture((int) gradientPreviewRect.width, (int) gradientPreviewRect.height));
+
+        for (int i = 0; i < gradient.NumKeys(); i++)
+        {
+            CustomGradient.ColourKey key = gradient.GetKey(i);
+            Rect keyRect = new Rect(gradientPreviewRect.x + gradientPreviewRect.width * key.Time - keyWidth / 2f, gradientPreviewRect.yMax + borderSize, keyWidth, keyHeight);
+            EditorGUI.DrawRect(keyRect, key.Colour);
+        }
+
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0)
+        {
+            Color randomColor = new Color(Random.value, Random.value, Random.value);
+            float keyTime = Mathf.InverseLerp(gradientPreviewRect.x, gradientPreviewRect.xMax, guiEvent.mousePosition.x);
+            gradient.AddKey(randomColor, keyTime);
+            Repaint();
+        }
     }
 
     public void SetGradient(CustomGradient gradient)
--- Gradient Editor/Assets/Scripts/CustomGradient.cs
+++ Gradient Editor/Assets/Scripts/CustomGradient.cs
@@ -5,9 +5,53 @@ using UnityEngine;
 [System.Serializable]
 public class CustomGradient
 {
+    [SerializeField]
+    List<ColourKey> keys = new List<ColourKey>();
+
     public Color Evaluate(float time)
     {
-        return Color.Lerp(Color.white, Color.black, time);
+        if (keys.Count == 0)
+            return Color.white;
+
+        ColourKey keyLeft = keys[0];
+        ColourKey keyRight = keys[keys.Count - 1];
+
+        for (int i = 0; i < keys.Count - 1; i++)
+        {
+            if (keys[i].Time <= time && keys[i].Time >= time)
+            {
+                keyLeft = keys[i];
+                keyRight = keys[i + 1];
+                break;
+            }
+        }
+
+        float blendTime = Mathf.InverseLerp(keyLeft.Time, keyRight.Time, time);
+        return Color.Lerp(keyLeft.Colour, keyRight.Colour, blendTime);
+    }
+
+    public void AddKey(Color colour, float time)
+    {
+        ColourKey newKey = new ColourKey(colour, time);
+        for (int i = 0; i < keys.Count; i++)
+        {
+            if (newKey.Time < keys[i].Time)
+            {
+                keys.Insert(i, newKey);
+                return;
+            }
+        }
+        keys.Add(newKey);
+    }
+
+    public int NumKeys()
+    {
+        return keys.Count;
+    }
+
+    public ColourKey GetKey(int i)
+    {
+        return keys[i];
     }
 
     public Texture2D GetTexture(int width, int height)
@@ -21,5 +65,36 @@ public class CustomGradient
         texture.SetPixels(colors);
         texture.Apply();
         return texture;
+    }
+
+    [System.Serializable]
+    public struct ColourKey
+    {
+        [SerializeField]
+        Color colour;
+        [SerializeField]
+        float time;
+
+        public ColourKey(Color colour, float time)
+        {
+            this.colour = colour;
+            this.time = time;
+        }
+
+        public Color Colour
+        {
+            get
+            {
+                return colour;
+            }
+        }
+
+        public float Time
+        {
+            get
+            {
+                return time;
+            }
+        }
     }
 }
